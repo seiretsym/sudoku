fa6a27af0ed4a987e661626dca51d44262d6b3db
Kerwin Hy <kerwin.hy@gmail.com>
2020-10-11 10:39:53 -0700

    rework some algorithms for generating sudoku grid

--- script.js
+++ script.js
@@ -1,41 +1,83 @@
 function makeGrid(sudoku) {
   const grid = $("<div>").addClass("grid");
-  const gridCol1 = makeCol();
-  const gridCol2 = makeCol();
-  const gridCol3 = makeCol();
-
-  sudoku.forEach((array, index) => {
-    const box = makeBox();
-    const boxCol1 = makeCol();
-    const boxCol2 = makeCol();
-    const boxCol3 = makeCol();
-    for (let i = 0; i < 9; i++) {
+  const col = makeCol();
+  sudoku.forEach(array => {
+    const row = makeRow();
+    for (let i = 0; i < array.length; i++) {
       const cell = makeCell(array[i]);
-      if (i % 3 === 0) {
-        boxCol1.append(cell);
-      } else if (i % 3 === 1) {
-        boxCol2.append(cell);
-      } else {
-        boxCol3.append(cell);
-      }
+      row.append(cell);
     }
-    box.append(boxCol1, boxCol2, boxCol3)
-    if (index % 3 === 0) {
-      gridCol1.append(box);
-    } else if (index % 3 === 1) {
-      gridCol2.append(box);
-    } else {
-      gridCol3.append(box);
-    }
+    col.append(row);
   })
 
-  grid.append(gridCol1, gridCol2, gridCol3)
+  grid.append(col);
   $("#content").append(grid);
 }
 
-function makeBox() {
-  const box = $("<div>").addClass("box");
+function getBox(row, col, grid) {
+  const box = [];
+  // box 1
+  if (row < 3 && col < 3) {
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 3; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row > 2 && row < 6 && col < 3) { // box 2
+    for (let r = 3; r < 6; r++) {
+      for (let c = 0; c < 3; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row > 5 && col < 3) { // box 3
+    for (let r = 6; r < 9; r++) {
+      for (let c = 0; c < 3; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row < 3 && col > 2 && col < 6) { // box 4
+    for (let r = 0; r < 3; r++) {
+      for (let c = 3; c < 6; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row > 2 && row < 6 && col > 2 && col < 6) { // box 5
+    for (let r = 3; r < 6; r++) {
+      for (let c = 3; c < 6; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row > 5 && col > 2 && col < 6) { // box 6
+    for (let r = 6; r < 9; r++) {
+      for (let c = 3; c < 6; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row < 3 && col > 5) { // box 7
+    for (let r = 0; r < 3; r++) {
+      for (let c = 6; c < 9; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else if (row > 2 && row < 6 && col > 5) { // box 8 
+    for (let r = 3; r < 6; r++) {
+      for (let c = 6; c < 9; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  } else { // box 9
+    for (let r = 6; r < 9; r++) {
+      for (let c = 6; c < 9; c++) {
+        box.push(grid[r][c]);
+      }
+    }
+  }
   return box;
+}
+
+function makeRow() {
+  const row = $("<row>").addClass("row");
+  return row;
 }
 
 function makeCol() {
@@ -46,6 +88,17 @@ function makeCol() {
 function makeCell(num) {
   const cell = $("<div>").addClass("cell").text(num);
   return cell;
+}
+
+function checkGrid(grid) {
+  for (let rows = 0; rows < 9; rows++) {
+    for (let cols = 0; cols < 9; cols++) {
+      if (grid[rows][cols] === 0) {
+        return false
+      }
+    }
+  }
+  return true;
 }
 
 function makeSudoku() {
@@ -61,133 +114,36 @@ function makeSudoku() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]
 
-  // first let's pick a random box in the template
-  // const rng = Math.floor(Math.random() * 9);
-  const rng = 0;
+  // total cells = 81 so loop 81 times
+  for (let cell = 0; cell < 81; cell++) {
+    // determine row and col
+    const row = Math.floor(i / 9);
+    const col = i % 9;
 
-  // then we set up the box with 1-9 and shuffle it around
-  let box = [1,2,3,4,5,6,7,8,9];
-  box = shuffle(box);
-
-  // setup the first box in the grid
-  template[rng] = box;
-
-  // let's set up our rows and cols to make placement easier
-  const rowGrid = convertGrid(template, "row");
-  const colGrid = convertGrid(template, "col");
-
-  // start setting up sudoku grid
-  for (let i = 0; i < template[rng].length; i++) {
-    let row = 0;
-    let col = 0;
-    if (rng < 3) {
-      if (rng % 3 === 0) {
-        if (i < 3) {
-          row = 0;
-          rows = insertCellByRow(template[rng][i], row, rowGrid);
-        } else if (i < 6) {
-          row = 1;
-          // rows = insertCellByRow(template[rng][i], row, rowGrid);
+    if (template[row][col] === 0) {
+      // loop through each number used in sudoku 1-9
+      for (let num = 1; num < 10; num++) {
+        // check if number has been used in row
+        if (template[row].indexOf(num) > -1) {
+          continue;
         } else {
-          row = 2;
-          // rows = insertCellByRow(template[rng][i], row, rowGrid);
+          // check if number has been used in col
+          let checkCol = false;
+          for (let c = 0; c < 9; c++) {
+            if (template[row][c] === num) {
+              checkCol = true;
+              break;
+            } else {
+              // check the box we're working on
+            }
+          }
         }
-        // console.log("rows 1-3 & cols 1-3")
-      } else if (rng % 3 === 1) {
-        // console.log("rows 1-3 & cols 4-6")
-      } else {
-        // console.log("rows 1-3 & cols 7-9")
-      }
-    } else if (rng < 6) {
-      if (rng % 3 === 0) {
-        // console.log("rows 4-6 & cols 1-3")
-      } else if (rng % 3 === 1) {
-        // console.log("rows 4-6 & cols 4-6")
-      } else {
-        // console.log("rows 4-6 & cols 7-9")
-      }
-    } else {
-      if (rng % 3 === 0) {
-        // console.log("rows 7-9 & cols 1-3")
-      } else if (rng % 3 === 1) {
-        // console.log("rows 7-9 & cols 4-6")
-      } else {
-        // console.log("rows 7-9 & cols 7-9")
       }
     }
-
   }
 
-  template = convertGrid(rows, "row")
   console.log(template);
-  console.log(Array.from(template[0], (data, i) => i < 3))
   return template;
-}
-
-function insertCellByRow(num, row, grid) {
-  if (row < 3) {
-    if (row % 3 === 0) {
-      console.log("?");
-      while (grid[row+1].indexOf(num) === -1) {
-        const rng = Math.floor(Math.random() * 3) + 3;
-        if (grid[row+1][rng] === 0) {
-          grid[row+1][rng] = num;
-        }
-      }
-      while (grid[row+2].indexOf(num) === -1) {
-        const rng = Math.floor(Math.random() * 3) + 6;
-        if (grid[row+2][rng] === 0) {
-          grid[row+2][rng] = num;
-        }
-      }
-    } else if (row % 3 === 1) {
-      console.log("?");
-      while (grid[row-1].indexOf(num) === -1) {
-        const rng = Math.floor(Math.random() * 3) + 3;
-        if (grid[row-1][rng] === 0) {
-          grid[row-1][rng] = num;
-        } else {
-          grid[roq-1][rng] = 0;
-        }
-      }
-      while (grid[row+1].indexOf(num) === -1) {
-        const rng = Math.floor(Math.random() * 3) + 6;
-        if (grid[row+1][rng] === 0) {
-          grid[row+1][rng] = num;
-        }
-      }
-    } else {
-      while (grid[row-1].indexOf(num) === -1) {
-        const rng = Math.floor(Math.random() * 3) + 3;
-        if (grid[row-1][rng] === 0) {
-          grid[row-1][rng] = num;
-        }
-      }
-      while (grid[row-2].indexOf(num) === -1) {
-        const rng = Math.floor(Math.random() * 3) + 6;
-        if (grid[row-2][rng] === 0) {
-          grid[row-2][rng] = num;
-        }
-      }
-    }
-  } else if (row < 6) {
-        if (row % 3 === 0) {
-
-    } else if ( row % 3 === 1) {
-      
-    } else {
-      
-    }
-  } else {
-    if (row % 3 === 0) {
-
-    } else if ( row % 3 === 1) {
-      
-    } else {
-      
-    }
-  }
-  return grid;
 }
 
 function shuffle(array) {
@@ -200,108 +156,6 @@ function shuffle(array) {
     }
   }
   return array;
-}
-
-function convertGrid(grid, type) {
-  let temp = [[],[],[],[],[],[],[],[],[]];
-
-  switch(type) {
-    case "row":
-      grid.forEach((box, bIndex) => {
-        box.forEach((cell, cIndex) => {
-          if (Math.floor(bIndex / 3) === 0) {
-            if (cIndex < 3) {
-              temp[0].push(cell);
-            } else if (cIndex < 6) {
-              temp[1].push(cell);
-            } else {
-              temp[2].push(cell);
-            }
-          } else if (Math.floor(bIndex / 3) === 1) {
-            if (cIndex < 3) {
-              temp[3].push(cell);
-            } else if (cIndex < 6) {
-              temp[4].push(cell);
-            } else {
-              temp[5].push(cell);
-            }
-          } else {
-            if (cIndex < 3) {
-              temp[6].push(cell);
-            } else if (cIndex < 6) {
-              temp[7].push(cell);
-            } else {
-              temp[8].push(cell);
-            }
-          }
-        })
-      })
-      break;
-    case "col":
-      grid.forEach((box, bIndex) => {
-        box.forEach((cell, cIndex) => {
-          if (bIndex % 3 === 0) {
-            if (cIndex % 3 === 0) {
-              temp[0].push(cell);
-            } else if (cIndex % 3 === 1) {
-              temp[1].push(cell);
-            } else {
-              temp[2].push(cell);
-            }
-          } else if (bIndex % 3 === 1) {
-            if (cIndex % 3 === 0) {
-              temp[3].push(cell);
-            } else if (cIndex % 3 === 1) {
-              temp[4].push(cell);
-            } else {
-              temp[5].push(cell);
-            }
-          } else {
-            if (cIndex % 3 === 0) {
-              temp[6].push(cell);
-            } else if (cIndex % 3 === 1) {
-              temp[7].push(cell);
-            } else {
-              temp[8].push(cell);
-            }
-          }
-        })
-      })
-      break;
-    default:
-      temp = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
-      grid.forEach((col, coIndex) => {
-        for (let i = 0; i < 9; i++) {
-          if (coIndex < 3) {
-            if (i < 3) {
-              temp[0][(i%3*3) + (coIndex % 3)] = col[i];
-            } else if (i < 6) {
-              temp[3][(i%3*3) + (coIndex % 3)] = col[i];
-            } else {
-              temp[6][(i%3*3) + (coIndex % 3)] = col[i];
-            }
-          } else if (coIndex < 6) {
-            if (i < 3) {
-              temp[1][(i%3*3) + (coIndex % 3)] = col[i];
-            } else if (i < 6) {
-              temp[4][(i%3*3) + (coIndex % 3)] = col[i];
-            } else {
-              temp[7][(i%3*3) + (coIndex % 3)] = col[i];
-            }
-          } else {
-            if (i < 3) {
-              temp[2][(i%3*3) + (coIndex % 3)] = col[i];
-            } else if (i < 6) {
-              temp[5][(i%3*3) + (coIndex % 3)] = col[i];
-            } else {
-              temp[8][(i%3*3) + (coIndex % 3)] = col[i];
-            }
-          }
-        }
-      })
-      break;
-  }
-  return temp;
 }
 
 const sudoku = makeSudoku();
